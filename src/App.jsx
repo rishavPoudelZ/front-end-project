075dadcde4758e11989a57c7ea5210d8ceb5d013
Rishav Poudel <poudelrishav88@Gmail.com>
2024-02-02 16:33:17 +0545

    complete blog and hero section, start product section, of homepage

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,10 @@ import { useState } from 'react'
 import { Dialog } from '@headlessui/react'
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
 import "../src/App.css";
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
 
 const navigation = [
   { name: 'Product', href: '#' },
@@ -10,39 +14,85 @@ const navigation = [
   { name: 'Company', href: '#' },
 ]
 
-const products = [
+const articles = [
   {
     id: 1,
     name: 'Earthen Bottle',
     href: '#',
     price: '$48',
-    imageSrc: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-01.jpg',
+    imageSrc: './src/assets/1674705497799.png',
     imageAlt: 'Tall slender porcelain bottle with natural clay textured body and cork stopper.',
+    text:'Zephyrus vs Strix vs Flow: Whats the difference between ROG laptops?'
   },
   {
     id: 2,
     name: 'Nomad Tumbler',
     href: '#',
     price: '$35',
-    imageSrc: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-02.jpg',
+    imageSrc: './src/assets/166864109730.png',
     imageAlt: 'Olive drab green insulated bottle with flared screw lid and flat top.',
+    text:'How to set up and optimize your new ROG gaming laptop'
+
   },
   {
     id: 3,
     name: 'Focus Paper Refill',
     href: '#',
     price: '$89',
-    imageSrc: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-03.jpg',
+    imageSrc: './src/assets/w600.png',
     imageAlt: 'Person using a pen to cross a task off a productivity paper card.',
+    text:'ROG Flow Z13-ACRNM RMT02'
+
   },
   {
     id: 4,
     name: 'Machined Mechanical Pencil',
     href: '#',
     price: '$35',
-    imageSrc: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-04.jpg',
+    imageSrc: './src/assets/1654040596988.png',
     imageAlt: 'Hand holding black machined steel mechanical pencil with brass tip and top.',
+    text:'Unbridled gaming power and unlimited versatility: hands-on with the ROG Flow Z13'
   }
+]
+
+const products = [
+  {
+    id: 1,
+    name: 'Basic Tee',
+    href: '#',
+    imageSrc: './src/assets/w273.png',
+    imageAlt: "Front of men's Basic Tee in black.",
+    price: '$35',
+    color: 'Black',
+  },
+  {
+    id: 1,
+    name: 'Basic Tee',
+    href: '#',
+    imageSrc: './src/assets/w2737.png',
+    imageAlt: "Front of men's Basic Tee in black.",
+    price: '$35',
+    color: 'Black',
+  },
+  {
+    id: 1,
+    name: 'Basic Tee',
+    href: '#',
+    imageSrc: './src/assets/w27300.png',
+    imageAlt: "Front of men's Basic Tee in black.",
+    price: '$35',
+    color: 'Black',
+  },
+  {
+    id: 1,
+    name: 'Basic Tee',
+    href: '#',
+    imageSrc: './src/assets/w273111.png',
+    imageAlt: "Front of men's Basic Tee in black.",
+    price: '$35',
+    color: 'Black',
+  }
+  // More products...
 ]
 
 export default function Example() {
@@ -136,19 +186,8 @@ export default function Example() {
             </Dialog.Panel>
           </Dialog>
         </header>
-        <div className="relative isolate px-6 pt-56 md:pt-26 lg:px-8 lg:pt-1">
-          <div
-            className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
-            aria-hidden="true"
-          >
-            <div
-              className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
-              style={{
-                clipPath:
-                  'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
-              }}
-            />
-          </div>
+        <div className='container center borderS'>
+        <div className="relative">
           <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
             <div className="hidden sm:mb-8 sm:flex sm:justify-center">
               <div className="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
@@ -179,39 +218,62 @@ export default function Example() {
               </div>
             </div>
           </div>
-          <div
-            className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
-            aria-hidden="true"
-          >
-            <div
-              className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
-              style={{
-                clipPath:
-                  'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
-              }}
-            />
-          </div>
+        </div>
         </div>
       </div>
       <div className="backgroundRed">
-        <div className="mx-auto max-w-2xl px-0 py-2 sm:px-0 sm:py-4 lg:max-w-screen-2xl lg:px-4">
+        <div className="mx-auto max-w-2xl px-0 py-2 sm:px-0 sm:py-4 lg:max-w-screen-2xl lg:px-1">
           <h2 className="sr-only">Products</h2>
 
-          <div className="grid grid-cols-1 gap-y-1 gap-x-6 sm:grid-cols-2 sm:gap-y-2 lg:grid-cols-2 xl:grid-cols-2 xl:gap-x-2">
-            {products.map((product) => (
+          <div className="grid grid-cols-1 gap-y-1 gap-x-2 sm:grid-cols-2 sm:gap-y-2 lg:grid-cols-2 xl:grid-cols-2 xl:gap-x-2">
+            {articles.map((product) => (
               <a key={product.id} href={product.href} className="group">
-                <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
+                <div className="w-full h-full overflow-hidden bg-gray-200 relative">
                   <img
                     src={product.imageSrc}
                     alt={product.imageAlt}
                     className="h-full w-full object-cover object-center group-hover:opacity-75"
                   />
+                  <div className='absolute text-lg text-red-200 inset-3'>{product.text}</div>
                 </div>
               </a>
             ))}
           </div>
         </div>
       </div>
+      <div className="backgroundRed">
+      <div className="mx-auto max-w-2xl px-4 py-16 sm:px-2 sm:py-24 lg:max-w-7xl lg:px-2">
+        <h2 className="text-2xl font-bold tracking-tight text-gray-900">Products</h2>
+
+        <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+          {products.map((product) => (
+            <div key={product.id} className="group relative">
+              <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md lg:aspect-none group-hover:opacity-75 lg:h-80">
+                <img
+                  src={product.imageSrc}
+                  alt={product.imageAlt}
+                  className="h-full w-full object-cover object-center lg:h-full lg:w-full"
+                />
+              </div>
+              <div className="mt-4 flex justify-between">
+                <div>
+                  <h3 className="text-sm text-gray-700">
+                    <a href={product.href}>
+                      <span aria-hidden="true" className="absolute inset-0" />
+                      {product.name}
+                    </a>
+                  </h3>
+                  <p className="mt-1 text-sm text-gray-500">{product.color}</p>
+                </div>
+                <p className="text-sm font-medium text-gray-900">{product.price}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+
+      
     </>
   )
 }
